2b5880dcfc4ec07bdffa053b81255b0fe876ea63
SergSemak <semakin.s.v@gmail.com>
2023-02-26 00:56:23 +0300

    added methods for the first array

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+string[] GetArray(int size)
+{
+    string[] arr = new string[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{i}: ");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+int GetNumber(string text)
+{
+  Console.Write(text);
+  int number = int.Parse(Console.ReadLine());
+  return number;
+}
+
+int size = GetNumber("Enter length of array: ");
+Console.WriteLine("[" + String.Join(", ",GetArray(size)) + "]");
