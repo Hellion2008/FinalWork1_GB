6beb1c8070ac5b21205a223ac204907a5018c51e
SergSemak <semakin.s.v@gmail.com>
2023-02-26 16:48:17 +0300

    all code is done

--- Program.cs
+++ Program.cs
@@ -1,5 +1,18 @@
-﻿
-
+﻿string[] GetArrayWithThreeSymbols(string[] arr)
+{
+    string[] secondArr = new string[arr.Length];
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length < 3 )
+        {
+            secondArr[count] = arr[i];
+            count++;
+        }
+    }
+    Array.Resize(ref secondArr, count);
+    return secondArr;
+}
 
 string[] GetArray(int size)
 {
@@ -14,10 +27,12 @@ string[] GetArray(int size)
 
 int GetNumber(string text)
 {
-  Console.Write(text);
-  int number = int.Parse(Console.ReadLine());
-  return number;
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
 }
 
 int size = GetNumber("Enter length of array: ");
-Console.WriteLine("[" + String.Join(", ",GetArray(size)) + "]");
+string[] originalArray = GetArray(size);
+Console.Write("[" + String.Join(", ", originalArray) + "] -> ");
+Console.WriteLine("[" + String.Join(", ", GetArrayWithThreeSymbols(originalArray)) + "]");
